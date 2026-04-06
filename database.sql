27a0c0a0c7534d244b9b9b53248852c4e757a5e7
Artyom Rozhkov <argue364@gmail.com>
2026-04-06 16:55:37 +0200

    fix init of dbs

--- database.sql
+++ database.sql
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS url_checks (
     id SERIAL PRIMARY KEY,
     url_id INTEGER REFERENCES urls(id),
     status_code INTEGER,
-    h1 VARCHAR(255),
-    title VARCHAR(255),
-    description VARCHAR(255),
+    h1 TEXT,
+    title TEXT,
+    description TEXT,
     created_at TIMESTAMP
 );
