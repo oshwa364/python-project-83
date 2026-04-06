9c535407c89f6ba35ef6eced57937f5c150f9cac
Artyom Rozhkov <argue364@gmail.com>
2026-04-06 17:00:44 +0200

    fix add incorrect url

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -24,7 +24,7 @@ def add_url():
     raw_url = request.form['url']
     if not validators.url(raw_url):
         flash('Некорректный URL', 'alert-danger')
-        return redirect(url_for('index'))
+        return redirect(url_for('index')), 422
 
     normalized_url = normalize_url(raw_url)
 
